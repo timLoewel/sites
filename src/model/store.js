d048474eb8844953916a0f1147a48d4653467ff6
tim <tim@loewel.eu>
2017-06-13 17:54:14 +0200

    add simple versioning for store

--- src/model/store.js
+++ src/model/store.js
@@ -1,25 +1,28 @@
 import {AsyncStorage} from 'react-native';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware, compose, Store} from 'redux';
 import {persistStore, autoRehydrate} from 'redux-persist';
+import * as env from '../../env';
 
 import { createEpicMiddleware } from 'redux-observable';
 import { reduxFormMiddleware } from 'redux-form-actions';
-
 
 import reducer from './reducer';
 import rootEpic from './epics';
 import { createAction, createReducer } from 'redux-act';
 import immutableTransform from 'redux-persist-transform-immutable';
 
-
+const SCHEMA_VERSION = '0.1';
+const SCHEMA_VERSION_KEY = env.APP_NAME + ':SchemaVersion'
 let persistor;
 
 export const storeInitialized = createAction('storeInitialized: store has been initialized');
 
-export default function configureStore(onCompletion: ()=>void): any {
-
+export default async function configureStore(onCompletion: ()=>void): Store {
+	console.log('current schema version: ', SCHEMA_VERSION);
+	const schemaVersionStored = await AsyncStorage.getItem(SCHEMA_VERSION_KEY);
+	console.log('schema version stored: ', schemaVersionStored);
 	const enhancer = composeWithDevTools(
 			applyMiddleware(
 					reduxFormMiddleware,
@@ -31,9 +34,11 @@ export default function configureStore(onCompletion: ()=>void): any {
 	const store = createStore(reducer, undefined, enhancer);
 	persistor = persistStore(store, {storage: AsyncStorage, blacklist: ['ui', 'serverSocket', 'systemState'], transforms: [immutableTransform()]}, onCompletion);
 
-	//TODO TL remove this line,
-	// persistor.purge();
-	persistor.rehydrate();
+	if (schemaVersionStored !== SCHEMA_VERSION) {
+		await persistor.purge();
+		AsyncStorage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION);
+	}
+
 	return store;
 }
 
